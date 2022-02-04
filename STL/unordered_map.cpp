f7368f70db9bc94a815ad6b3ad300b26596ada53
Bhoomika Sahu <bhoomikasahu0010@gmail.com>
2022-02-04 12:47:29 +0530

    solving doubt in map

--- STL/unordered_map.cpp
+++ STL/unordered_map.cpp
@@ -2,26 +2,26 @@
 #include <unordered_map>
 using namespace std;
 
-void read(unordered_map<int, string> &m, int n){
-    for(int i=0; i<n; i++){
-        int x; string s;
-        cin >> x >> s;
-        m.insert(make_pair(x, s));
-    }
-    cout << endl;
-}
+// void read(unordered_map<int, string> &m, int n){
+//     for(int i=0; i<n; i++){
+//         int x; string s;
+//         cin >> x >> s;
+//         m.insert(make_pair(x, s));
+//     }
+//     cout << endl;
+// }
 
-void print(unordered_map<int, string> &m){
-    for(auto val : m){
-        cout << val.first << " " << val.second << endl;
-    }
-    cout << endl;
-}
+// void print(unordered_map<int, string> &m){
+//     for(auto val : m){
+//         cout << val.first << " " << val.second << endl;
+//     }
+//     cout << endl;
+// }
 
 int main(){
-    unordered_map<string, pair<int,int>> m = {{"abc",{1,2}}, {"ddd",{3,4}}};
-    auto it = m.begin();
-    cout << (*it).first << " " << ((*it).second).first << " " << ((*it).second).second;
+    // unordered_map<string, pair<int,int>> m = {{"abc",{1,2}}, {"ddd",{3,4}}};
+    // auto it = m.begin();
+    // cout << (*it).first << " " << ((*it).second).first << " " << ((*it).second).second;
     // int n;
     // cin >> n;
     // m[2] = "Bhoomika";   // O(1)
@@ -34,4 +34,13 @@ int main(){
     // cout << "erase(3) " << endl;
     // m.erase(it);
     // print(m);
+
+    unordered_map<char, int> m = {{'a', 1}, {'b', 2}, {'c', 3}};
+    string s = "abc";
+    
+    for(char c : s){
+        if(m[c] > 0){
+            cout << "yes";
+        }
+    }
 }
