159d99eacc70f119c7ed428807069bba01343106
Bhoomika Sahu <bhoomikasahu0010@gmail.com>
2024-01-10 14:12:18 +0530

    Iterator

--- STL/iterator.cpp
+++ STL/iterator.cpp
@@ -12,16 +12,16 @@ int main(){
     //     cin >> ele;
     //     v.push_back(ele);
     // }
-    cout << *(v.begin()) << " " << *(v.end());
-    // vector<int>::iterator itr1;
+    // cout << *(v.begin()) << " " << *(v.end());
+    vector<int>::iterator itr1;
     // for(itr1=v.begin(); itr1!=v.end(); itr1++){
     //     cout << *itr1 << " ";
     // }
 
 
-    // itr1 = v.end();
+    itr1 = v.end()-1;
     // itr1++;
-    // cout << *itr1 << endl;
+    cout << *itr1 << endl;
     // vector<int>::iterator itr2;
     // itr2 = v.begin();
     // int count = itr2-itr1;
