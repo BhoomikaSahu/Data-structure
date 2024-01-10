580a379a360bd2dca643628a38c530af5ecf6569
Bhoomika Sahu <bhoomikasahu0010@gmail.com>
2024-01-10 14:08:15 +0530

    Insertion Sort

--- Array/Sorting/insertionSort.cpp
+++ Array/Sorting/insertionSort.cpp
@@ -18,20 +18,29 @@ void print (int arr[], int n){
     }
 }
 void insertionSort( int arr[], int n ){
-	for(int i=1; i<n; i++)
-    {
-        int current = arr[i];
-        int j = i-1;
-        /* Move elements of arr[0..i-1], that are 
-        greater than key, to one position ahead 
-        of their current position */
-        while(j>=0 && current<arr[j])
-        {
-            arr[j+1] = arr[j];
-            j--;
+    for(int i=0; i<n; i++) {
+        for(int j=i; j>0; j--) {
+            if(j>0 && arr[j]<arr[j-1]){
+                int temp = arr[j];
+                arr[j] = arr[j-1];
+                arr[j-1] = temp;
+            }
         }
-        arr[j+1] = current;
     }
+	// for(int i=1; i<n; i++)
+    // {
+    //     int current = arr[i];
+    //     int j = i-1;
+    //     /* Move elements of arr[0..i-1], that are 
+    //     greater than key, to one position ahead 
+    //     of their current position */
+    //     while(j>=0 && current<arr[j])
+    //     {
+    //         arr[j+1] = arr[j];
+    //         j--;
+    //     }
+    //     arr[j+1] = current;
+    // }
 }
 
 int main()
